7fe98095f60f2c4a3a20c5d953101cc47740f8f3
Julia Aptekar <japtekar2024@gmail.com>
2026-06-02 22:46:51 -0700

    yay

--- R/data.R
+++ R/data.R
@@ -1,10 +1,9 @@
-#' Load bundled Pokémon TCG dataset
+#'Load bundled Pokémon TCG dataset
 #'
 #' @return A tibble containing Pokémon TCG data.
+#' @importFrom arrow read_csv_arrow
 #' @export
 load_data <- function() {
-
-  path <- "https://www.dropbox.com/scl/fi/tnl4wcmgduu3bnmmllz2u/pokemon_cards.csv?rlkey=h7evg3hr4ckzqrxrzoy458ojs&st=uzrlktbc&dl=1"
-  readr::read_csv(path, show_col_types = FALSE)
-
+ arrow::read_csv_arrow(
+   "https://www.dropbox.com/scl/fi/tnl4wcmgduu3bnmmllz2u/pokemon_cards.csv?rlkey=h7evg3hr4ckzqrxrzoy458ojs&st=uzrlktbc&dl=1")
 }
